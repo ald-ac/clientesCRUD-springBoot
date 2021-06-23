184d4060dfb510d2657815ad9d572f4354a3ed4e
ald-ac <muriasaldair@hotmail.com>
2021-06-23 17:54:43 -0500

    Primer commit -  Listado de clientes de la BD en un tabla HTML

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id, nombre, apellido, email, creado_en) VALUES (1, 'Jose', 'Acosta', 'jose@gmail.com', '2021-05-23');
+INSERT INTO clientes (id, nombre, apellido, email, creado_en) VALUES (2, 'Aldair', 'Murias', 'aldair@gmail.com', '2021-05-24');
