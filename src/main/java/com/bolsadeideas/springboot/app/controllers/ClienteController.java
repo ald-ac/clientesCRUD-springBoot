28c6190b3e6667af05633ae99edcb02d4670b125
ald-ac <muriasaldair@hotmail.com>
2021-07-05 19:57:37 -0500

    Agregando dependencia de validacion, validando campos clase entity y a su vez notificando esos posibles errores en plantilla form.html

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,8 +1,11 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -37,7 +40,12 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value = "/agregar", method = RequestMethod.POST)
-	public String guardar(Cliente cliente) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("titulo", "Agregar nuevo cliente");
+			return "form";
+		}
+		
 		clienteDao.agregar(cliente);
 		return "redirect:listar";
 	}
